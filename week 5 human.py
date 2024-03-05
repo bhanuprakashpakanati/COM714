ba3b8daee02143329c5651f994f2037e2285d522
bhanuprakashpakanati <159918438+bhanuprakashpakanati@users.noreply.github.com>
2024-03-05 10:28:39 +0000

    Add files via upload

---
+++ week 5 human.py
@@ -0,0 +1,16 @@
+from animal import Animal
+from clothing import Clothing
+
+class Human(Animal):
+    MOVE_ENERGY = 10
+    REPRODUCE_ENERGY = 20
+
+    def __init__(self, name:str, age:int=0, energy:int=Animal.MAX_ENERGY) -> None:
+        super(Human, self).__init__(name, age, energy)
+        self.clothing = []
+
+    def dress(self, clothing:Clothing):
+        self.clothing.add(clothing)
+
+    def undress(self, clothing:Clothing):
+        self.clothing.remove(clothing)
---
+++ week 6 super_human.py
@@ -0,0 +1,18 @@
+from flying_super_power import FlyingSuperPower
+from human import Human
+from invisibility_super_power import InvisibilitySuperPower
+
+
+class SuperHuman(Human, FlyingSuperPower, InvisibilitySuperPower):
+
+    def __init__(self):
+        pass
+
+    def fly(self):
+        print("I am flying!!!")
+
+    def turn_invisible(self):
+        print("I am now invisible")
+
+    def turn_visible(self):
+        print("I am now visible")
